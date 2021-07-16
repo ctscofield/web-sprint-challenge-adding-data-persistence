e8d3e356e886198019133ea350237cb7d3c7ae32
ctscofield <ctscofield@gmail.com>
2021-07-16 17:12:53 -0600

    ffffffff

--- api/task/model.js
+++ api/task/model.js
@@ -2,11 +2,40 @@
 const db = require('./../../data/dbConfig')
 
 function find() {
-  return db('tasks')
+  return db('tasks as t')
+    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select('t.*', 'p.project_name', 'p.project_description')
 }
 
-function add(task) {
-  return db('tasks').insert(task)
+async function add(task) {
+  const [task_id] = await db('tasks').insert(task)
+  console.log(task_id)
+  return find().where({ task_id }).first()
 }
 
-module.exports = {find, add}
+async function findById(task_id) {
+  const rows = await db('tasks as t')
+    .where('task_id', task_id)
+    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select('t.*', 'p.project_name', 'p.project_description')
+
+  const result = {
+    task_id: rows[0].task_id,
+    task_description: rows[0].task_description,
+    task_notes: rows[0].task_notes,
+    task_completed: rows[0].task_completed,
+    projects: []
+  }
+  rows.forEach(row => {
+    if (row.project_id) {
+      result.projects.push({
+        project_id: row.project_id,
+        project_name: row.project_name,
+        project_description: row.project_description
+      })
+    }
+  })
+  return result
+}
+
+module.exports = {find, add, findById}
--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,51 @@
-// build your `/api/tasks` router here
-`[GET] /api/tasks`
+// build your `/api/projects` router here
+const express = require('express')
+const Tasks = require('./model')
+
+const router = express.Router()
 
 
-`[POST] /api/tasks`
+// `[GET] /api/tasks`
+router.get('/', (req, res, next) => {
+  Tasks.find()
+    .then(tasks => {
+      if (!tasks) {
+        res.status(404).json({
+          message: "can't find task"
+        })
+      } else {
+        res.json(tasks)
+      }
+    })
+    .catch(next)
+})
+
+// `[POST] /api/tasks`
+router.post('/', (req, res, next) => {
+  const {task_description, task_notes, task_completed} = req.body
+  console.log(req.body)
+  if (task_description === undefined || typeof task_description !== 'string' || !task_description.trim()) {
+    res.status(400).json({
+      message: "missing task description"
+    })
+  } else {
+    Tasks.add({task_description, task_notes, task_completed})
+      .then(({task_id}) => {
+        return Tasks.findById(task_id)
+      })
+      .then(task => {
+        res.status(201).json(task)
+      })
+      .catch(next)
+  }
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    sageAdvice: 'Finding the real error is 90% of the bug fix',
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
+module.exports = router;
